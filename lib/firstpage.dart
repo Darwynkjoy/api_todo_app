b891e7cfb46ba332f92c571526f0200401bed540
darwynkjoy <darwynkjoy@gmail.com>
2025-01-15 11:57:10 +0530

    daily push

--- lib/firstpage.dart
+++ lib/firstpage.dart
@@ -5,14 +5,15 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart'as http;
 
 class Firstpage extends StatefulWidget{
+  Firstpage({super.key});
   @override
   State<Firstpage> createState()=> _firstpageState();
 }
 
 class _firstpageState extends State<Firstpage>{
   bool _isLoading=true;
-
-  void initstate(){
+  @override
+  void initState(){
     super.initState();
     _getData();
   }
@@ -38,7 +39,7 @@ class _firstpageState extends State<Firstpage>{
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: Text("Todo app"),
+        title: Text("TODO APP",style: TextStyle(fontSize: 25,color: Colors.white,fontWeight: FontWeight.bold),),
         centerTitle: true,
         backgroundColor: Colors.amber,
       ),
@@ -56,13 +57,32 @@ class _firstpageState extends State<Firstpage>{
       )
       :ListView.separated(itemBuilder: (context,index){
         final data=dataFromAPI!.todos[index];
-        return ListTile(
-          title: Text("${data.id}"),
-
+        return Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Container(
+            height: 200,
+            width: 150,
+            decoration: BoxDecoration(border: Border.all(width: 2,color: Colors.amber),
+            borderRadius: BorderRadius.circular(15)),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("ID                    : ${data.id}",style: TextStyle(fontSize: 20,color: Colors.black,fontWeight: FontWeight.bold),),
+                  Text("TODO             : ${data.todo}",style: TextStyle(fontSize: 20,color: Colors.black,fontWeight: FontWeight.bold),),
+                  Text("USER ID         : ${data.userId}",style: TextStyle(fontSize: 20,color: Colors.black,fontWeight: FontWeight.bold),),
+                  Text("COMPLETED: ${data.completed}",style: TextStyle(fontSize: 20,color: Colors.black,fontWeight: FontWeight.bold),),
+                  ],
+              ),
+            ),
+          ),
         );
       }, separatorBuilder: (context,index){
         return SizedBox(height: 5,);
-        }, itemCount: dataFromAPI!.todos.length)
+        }, itemCount: dataFromAPI!.todos.length,        
+        )
     );
   }
 }
